9a7b6069bda1be5fd2da192e8005c2ad3329507e
NPotokin <npotokin@gmail.com>
2023-01-27 07:58:31 -0700

    Set up styles

--- meta-little-lemon/tailwind.config.js
+++ meta-little-lemon/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'prim1': '#495e57',
+      'prim2': '#f4ce14',
+      'sec1': '#ee9972',
+      'sec2': '#fbdabb',
+      'hl1': '#edefee',
+      'hl2': '#333333',
+    },
+    fontFamily: {
+      sans: ['Karla', 'sans-serif'],
+      serif: ['Markazi', 'serif'],
     extend: {},
   },
   plugins: [],
+},
 }
